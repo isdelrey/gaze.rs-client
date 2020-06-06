9125496dfcb9090188e0bfdfa117c82eb8a8bbd5
Ivo Sequeros <ivosequeros@me.com>
2020-06-06 17:34:57 +0200

    Disabled full concurrency

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,6 @@ use futures::future::{join_all};
 
 #[tokio::main]
 async fn main() {
-    let mut handles = Vec::with_capacity(100);
-    /* Run actors: */
-    for _ in 1..1000 {
-        handles.push(tokio::spawn(actor::run()));
-    }
-
-    join_all(handles).await;
-
-    thread::park();
-
+    actor::run().await;
     ()
 }
